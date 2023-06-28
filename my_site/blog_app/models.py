77ecfb7dec6e8f45be17636af9c536c0bfd545ec
Vladislav Zalogin <buntzalogin@gmail.com>
2023-06-28 11:58:42 +0300

    add redaction

--- my_site/blog_app/models.py
+++ my_site/blog_app/models.py
@@ -6,17 +6,11 @@ class Blog(models.Model):
     """
     Модель блога. Может быть в активном или неактивном состоянии
     """
-    STATUS_CHOICES = [
-        ('active', 'Active'),
-        ('not_active', 'Not active')
-    ]
 
     title = models.CharField(max_length=100, verbose_name='Заголовок')
     body = models.CharField(max_length=1000, verbose_name='Содержание')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата редактирования')
-    active = models.CharField(max_length=10, choices=STATUS_CHOICES,
-                              default='active', verbose_name='Флаг активности')
 
     class Meta:
         ordering = ('created_at',)
--- my_site/blog_app/views.py
+++ my_site/blog_app/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render
 from django.views import generic, View
 from .models import Blog
@@ -30,8 +29,22 @@ class BlogCreationView(View):
         return render(request=request, template_name='blog_app/blog_creation.html',
                       context={'blog_result': blog_result})
 
-#  TODO: создать представление редактирования блога
 
+class BlogRedactionView(View):
+
+    def get(self, request, pk, *args, **kwargs):
+        blog_db = Blog.objects.get(id=pk)
+        blog_form = BlogForm(initial={'title': blog_db.title, 'body': blog_db.body})
+        return render(request=request, template_name='blog_app/blog_redaction.html',
+                      context={'blog_form': blog_form})
+
+    def post(self, request, pk, *args, **kwargs):
+        blog_db = Blog.objects.get(id=pk)
+        blog = BlogForm(data=request.POST, instance=blog_db)
+        blog.save()
+        blog_result = 'Блог отредактирован'
+        return render(request=request, template_name='blog_app/blog_redaction.html',
+                      context={'blog_result': blog_result})
 
 class MainView(generic.TemplateView):
     template_name = 'blog_app/main.html'
